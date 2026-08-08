0d0da981abaadd56611d839272b827da89d2f202
MohammedFurquan99 <furquan2580@gmail.com>
2026-08-08 09:51:39 -0700

    practice

---
+++ practicequestions/condtions.py
@@ -0,0 +1,9 @@
+name = (input("name:"))
+if(name == "furquan"):
+    print("data science")
+elif(name == "shoaib"):
+    print("AIML")
+elif(name == "sarmas"):
+    print("AI")
+else:
+    print("NO DATA")
