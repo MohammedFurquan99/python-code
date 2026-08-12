ad192b49e926f7cd1196dcc67ed0b78694d37688
MohammedFurquan99 <furquan2580@gmail.com>
2026-08-12 06:43:01 -0700

    practice

---
+++ practicequestions/conditions.py
@@ -0,0 +1,10 @@
+
+for marks in range(0, 101, 10):  
+    if marks >= 90:
+        print(marks, "→ Grade: A")
+    elif marks >= 75:
+        print(marks, "→ Grade: B")
+    elif marks >= 50:
+        print(marks, "→ Grade: C")
+    else:
+        print(marks, "→ Grade: Fail")
