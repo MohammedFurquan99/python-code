a2d066d90c6113b93123dd9ff15bc6d1b9524ff8
MohammedFurquan99 <furquan2580@gmail.com>
2026-08-13 03:28:28 -0700

    practice

---
+++ practicequestions/gpt.py
@@ -0,0 +1,9 @@
+numbers = [3, 7, 2, 9, 4]
+
+total = 0
+
+for n in numbers:
+    if n > 5:
+        total = total + n
+
+print(total)
---
+++ practicequestions/gpt1.py
@@ -0,0 +1,5 @@
+numbers = [10, 15, 20, 25, 30]
+
+for n in numbers:
+    if n % 5 == 0 and n > 20:
+        print(n)
